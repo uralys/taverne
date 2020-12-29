6a2b16697a6361dee368b315aeac8b007938e883
chrisdugne <chris.dugne@uralys.com>
2020-12-29 15:03:35 +0100

    exporting all tools

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import createStore from './create-store';
 import connectStore from './connect-store';
+import {Dispatcher, useDispatcher} from './dispatcher';
+import {StoresProvider, useStores} from './stores-provider';
 
-export {connectStore};
+export {
+  createStore,
+  connectStore,
+  Dispatcher,
+  StoresProvider,
+  useDispatcher,
+  useStores
+};
