2a6207b8b7168e5f3937e6f3822770e9c05ab12c
chrisdugne <chris.dugne@uralys.com>
2021-02-27 21:28:51 +0100

    releasing a folder as package

---
+++ scripts/build-package.sh
@@ -0,0 +1,8 @@
+echo '☢️  preparing package...'
+mkdir dist.package
+cp readme.md dist.package/.
+cp readme.md dist.package/.
+cp license dist.package/.
+cp package.json dist.package/.
+cp -r src/middlewares dist.package/.
+echo '☢️  dist.package ready.'
--- scripts/prepare-package.sh
+++
@@ -1,7 +0,0 @@
-
-echo '☢️  preparing package...'
-cp readme.md dist/.
-cp license dist/.
-cp package.json dist/.
-cp -r src/middlewares dist/.
-rm -rf dist/meta
