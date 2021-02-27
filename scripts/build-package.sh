fb11771704c4ae493f7613e38d6ee970ee1ac243
chrisdugne <chris.dugne@uralys.com>
2021-02-27 21:30:25 +0100

    added cjs and esm files to package

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -1,8 +1,9 @@
 echo '☢️  preparing package...'
 mkdir dist.package
-cp readme.md dist.package/.
+cp -r dist/cjs dist.package/.
+cp -r dist/esm dist.package/.
+cp -r src/middlewares dist.package/.
 cp readme.md dist.package/.
 cp license dist.package/.
 cp package.json dist.package/.
-cp -r src/middlewares dist.package/.
 echo '☢️  dist.package ready.'
